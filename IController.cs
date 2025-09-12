9a7eecfcdaf930f09226f0b0c866c8ffaf57b92a
black1172 <black.1172@buckeyemail.osu.edu>
2025-09-11 20:34:19 -0400

    added needed interfaces

---
+++ IController.cs
@@ -0,0 +1,5 @@
+public interface IController
+{
+	// Marker interface for input controllers
+	// Add methods for keyboard and mouse input handling as needed
+}
---
+++ ISprite.cs
@@ -0,0 +1,5 @@
+public interface ISprite
+{
+	// Marker interface for sprites
+	// Add properties/methods for moving/non-moving sprites as needed
+}

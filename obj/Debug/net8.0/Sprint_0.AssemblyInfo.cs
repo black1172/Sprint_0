71ccb9221018bb1f3049eb8e050a1d9b88a307fd
black1172 <black.1172@buckeyemail.osu.edu>
2025-09-11 20:47:36 -0400

    added key binds to mouse and keyboard

--- obj/Debug/net8.0/Sprint_0.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sprint_0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint_0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a7eecfcdaf930f09226f0b0c866c8ffaf57b92a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6883e64716137da3cb1609b2a76c77530d75c6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint_0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint_0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
